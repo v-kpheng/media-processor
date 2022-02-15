02afdb9433eaa596915b8b06f729269937328b1e
Jose Antonio Olivera Ortega <joseantonio.oliveraortega@vonage.com>
2022-02-15 11:56:27 +0100

    OW-105: Address review comments

--- lib/src/utils/utils.ts
+++ lib/src/utils/utils.ts
@@ -1,7 +1,7 @@
 export function isSupported(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         if ((typeof MediaStreamTrackProcessor === 'undefined') || (typeof MediaStreamTrackGenerator === 'undefined')) {
-            reject('Your browser does not support the experimental MediaStreamTrack API for Insertable Streams of Media.');
+            reject('Your browser does not support the MediaStreamTrack API for Insertable Streams of Media.');
         } else {
             resolve();
         }
